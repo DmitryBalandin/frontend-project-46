dba800c1d94c811cceb2fec34a9d4fc8309cede2
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-04-19 18:58:30 +0500

    9

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,9 +1,10 @@
 import path from 'path';
 import process from 'process';
 import yaml from 'js-yaml';
+import _ from 'lodash';
 
 export const findAbsolutePath = (pathFile) => path.resolve(process.cwd(), pathFile);
 
-export const sortAndCopyObject = (jsonFile) => Object.fromEntries(Object.entries(jsonFile).sort());
+export const sortAndCopyObject = (jsonFile) => Object.fromEntries(_.sortBy(Object.entries(jsonFile)));
 
 export const transformYamltoJSON = (file) => yaml.load(file);
